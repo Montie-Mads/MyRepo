e9eedef24e8251c8481155c2527d8a385f2fd4b9
Montie-Mads <mmadeline551@gmail.com>
2023-02-20 11:25:48 -0500

    memoryGame-update

--- GA practice/JSPractice-2/memoryMatch.js
+++ GA practice/JSPractice-2/memoryMatch.js
@@ -2,8 +2,21 @@ const cards = document.querySelectorAll('.memory-card');
 //cards is == to the memory card divs!
 //below we are making a function to make the cards FLIP when clicked
 
+let hasFlipped = false;
+let firstCard, secondCard;
+
 function flipCard() {
-    this.classList.toggle('flip');
+    // this.classList.toggle('flip');
+
+    //this takes place of toggle flip
+    this.classList.add('flip');
+
+    if (!hasFlippedCard){
+       
+        hasFlippedCard = true;
+        firstCard = this;
+    }
+
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
