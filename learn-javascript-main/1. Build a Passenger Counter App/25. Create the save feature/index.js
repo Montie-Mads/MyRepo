127528fa0a7253be1eb93b17dfa0a2a58f5def0d
Montie-Mads <mmadeline551@gmail.com>
2023-04-06 14:32:07 -0400

    Quizilla-side nav added

--- learn-javascript-main/1. Build a Passenger Counter App/25. Create the save feature/index.js
+++ learn-javascript-main/1. Build a Passenger Counter App/25. Create the save feature/index.js
@@ -1,4 +1,6 @@
 // 1. Grab the save-el paragrah and store it in a variable called saveEl
+let saveEl = document.getElementById("save-el")
+
 let countEl = document.getElementById("count-el")
 let count = 0
 
@@ -11,7 +13,14 @@ function save() {
     // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
     // 3. Render the variable in the saveEl using innerText
     // NB: Make sure to not delete the existing content of the paragraph
-    console.log(count)
+    
+    let Entries = count + " - "
+
+    saveEl.innerText += " " + Entries
+
+
+
+        console.log(count)
 }
 
 
