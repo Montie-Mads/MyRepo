2102cb236bb871cdc3ca67a6dbe73a9fe801c048
Montie-Mads <mmadeline551@gmail.com>
2023-08-01 20:36:39 -0400

    assignment 1 done

---
+++ completeJScourse2023/complete-javascript-course-master/02-Fundamentals-Part-2/starter/myJS.js
@@ -0,0 +1,21 @@
+// let js = "amazing"
+// console.log(40+8+23-10)
+
+// console.log('Jonas');
+// console.log(23);
+
+// let firstName = "Jonas";
+
+// console.log(firstName)
+
+// let lastName = "Smith"
+
+// console.log(firstName + ' ' + lastName)
+
+// let fullName = "Jonas Smith"
+
+// console.log(fullName)
+
+// function multiply(number) {
+//     return number * number;
+// }
